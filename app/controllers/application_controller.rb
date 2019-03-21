76d16d158b4e779c4cc2948226f1aadb97f13aaf
raiou <c.s.t0474981366@gmail.com>
2019-03-21 02:25:18 +0000

    follow, followers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ end
 
 def counts(user)
   @count_microposts = user.microposts.count
+  @count_followings = user.followings.count
+  @count_followers = user.followers.count
 end
 
 # ApplicationController にメソッドを定義すると、全ての Controller で定義したメソッドが使用できるようになります。なぜなら、全ての Controller が ApplicationController を継承しているからです。
